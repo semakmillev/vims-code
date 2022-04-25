36c16d431632b2da8e314b8428867c38ac14f647
semak <semakmillev@gmail.com>
2022-04-25 12:02:46 +0300

    first urls

--- vims_code/urls/__init__.py
+++ vims_code/urls/__init__.py
@@ -1,14 +1,19 @@
-# from vims_code.urls.admin import admin_routes
-# from vims_code.urls.entities import entity_routes
-# from vims_code.urls.game_calls import game_routes
+from vims_code.urls.admin import admin_routes
+from vims_code.urls.entities import entity_routes
+from vims_code.urls.game_calls import game_routes
 from vims_code.urls.auth_calls import auth_routes
 
 # from vims_code.urls.info_calls import info_routes
+from vims_code.urls.info_calls import info_routes
 
 LIST_OF_ROUTES = [
-                  auth_routes
-                  ]
-#admin_routes,
-                  # info_routes,
-                  # game_routes,
-                  # entity_routes
+    auth_routes,
+    info_routes,
+    game_routes,
+    entity_routes
+
+]
+# admin_routes,
+# info_routes,
+# game_routes,
+# entity_routes
